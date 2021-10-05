f9a0b865e3eeb017002c4e617ab2c9723c9d5d1b
Joshua Morgan <jmorg0605@gmail.com>
2021-10-05 17:37:03 -0400

    completed exercise

---
+++ js101_small_problems/02_easy2/11_convert_number_to_string.js
@@ -0,0 +1,40 @@
+/*
+In the previous two exercises, you developed functions that convert simple numeric
+strings to signed integers. In this exercise and the next, you're going to reverse
+those functions.
+
+Write a function that converts a non-negative integer value (e.g., 0, 1, 2, 3, and so
+  on) to the string representation of that integer.
+
+You may not use any of the standard conversion functions available in JavaScript, such
+as String(), Number.prototype.toString, or an expression such as '' + number. Your
+function should do this the old-fashioned way and construct the string by analyzing and manipulating the number.
+*/
+
+/*
+  set numbers var to empty array
+  divide number by 10
+    - set digit to remainder and push to numbers array
+    - reassign number to quotient.
+    stop when quotient equals 0
+*/
+
+const DIGITS = new Array('0','1','2','3','4','5','6','7','8','9');
+
+function integerToString(integer) {
+  let stringNum = ''
+
+  do {
+    let remainder = integer % 10;
+    integer = Math.floor(integer / 10);
+    stringNum = DIGITS[remainder] + stringNum;
+  } while (integer > 0);
+
+  return stringNum;
+}
+
+
+integerToString(4321);        // "4321"
+integerToString(0);           // "0"
+integerToString(5000);        // "5000"
+integerToString(1234567890);  // "1234567890"
--- js101_small_problems/02_easy2/12_convert_signed_number_to_string.js
+++ js101_small_problems/02_easy2/12_convert_signed_number_to_string.js
@@ -1,22 +1,55 @@
 /*
-Write a function that returns an Array that contains every other element of an Array
-that is passed in as an argument. The values in the returned list should be those
-values that are in the 1st, 3rd, 5th, and so on elements of the argument Array.
+In the previous exercise, you developed a function that converts non-negative numbers
+to strings. In this exercise, you're going to extend that function by adding the
+ability to represent negative numbers as well.
+
+Write a function that takes an integer and converts it to a string representation.
+
+You may not use any of the standard conversion functions available in JavaScript, such
+as String() and Number.prototype.toString(). You may, however, use integerToString() 
+from the previous exercise.
+
+You might also want to check the Math.sign() function.
 */
 
-/*
+const DIGITS = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 
+function integerToString(number) {
+  let result = '';
 
-*/
+  do {
+    let remainder = number % 10;
+    number = Math.floor(number / 10);
 
-
-function oddities(array) {
-  array.forEach(element => {
-    
-  });
+    result = DIGITS[remainder] + result;
+  } while (number > 0);
+ 
+  return result;
 }
-console.log(oddities([2, 3, 4, 5, 6])); // logs [2, 4, 6]
-console.log(oddities([1, 2, 3, 4, 5, 6])); // logs [1, 3, 5]
-console.log(oddities(["abc", "def"])); // logs ['abc']
-console.log(oddities([123])); // logs [123]
-console.log(oddities([])); // logs []
+
+function signedIntegerToString(number) {
+  if (number < 0) {
+    number = Math.abs(number)
+   return '-' + integerToString(number);
+  } else if (number > 0) {
+    return '+' + integerToString(number);
+  } else return "0"
+}
+
+// or
+
+function signedIntegerToString(number) {
+  switch (Math.sign(number)) {
+    case -1:
+      return `-${integerToString(-number)}`;
+    case +1:
+      return `+${integerToString(number)}`;
+    default:
+      return integerToString(number);
+  }
+}
+
+
+ console.log(signedIntegerToString(4321) === "+4321");
+console.log(signedIntegerToString(-123) === "-123");
+ console.log(signedIntegerToString(0) === "0");
