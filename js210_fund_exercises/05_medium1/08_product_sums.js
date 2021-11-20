838b6bcb8cce5214fac3a63632bdf6e3df6e23c3
Joshua Morgan <jmorg0605@gmail.com>
2021-11-20 16:19:00 -0500

    complete execise

--- js210_fund_exercises/05_medium1/08_product_sums.js
+++ js210_fund_exercises/05_medium1/08_product_sums.js
@@ -4,11 +4,6 @@ sums of two arrays of numbers. Read through the following code.
 Will it produce the expected result? Why or why not?
 */
 
-function productOfSums(array1, array2) {
-  let result = total(array1) * total(array2);
-  return result;
-}
-
 function total(numbers) {
   let sum;
 
@@ -18,3 +13,14 @@ function total(numbers) {
 
   sum;
 }
+
+function productOfSums(array1, array2) {
+  let result = total(array1) * total(array2);
+  return result;
+}
+
+/*
+line 8, sum is set to undefined, so when sum is incremented, JS
+won't know how to concat undefined with integer, producing NaN
+linke 14, all functions return a value and does so with keyword
+*/
