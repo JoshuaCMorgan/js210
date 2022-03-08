ef1b80055be836755d731e96ac675a905584f3fb
Joshua Morgan <jmorg0605@gmail.com>
2022-03-08 11:57:40 +0800

    completed exercise

--- js210_small_problems/08_medium1/06_fib_recursion.js
+++ js210_small_problems/08_medium1/06_fib_recursion.js
@@ -1,7 +1,7 @@
 
 
 function fibonacci(nth) {
-  if (n <= 2) {
+  if (nth <= 2) {
     return 1;
   } else {
     return fibonacci(nth - 1) + fibonacci(nth - 2);
@@ -11,7 +11,8 @@ function fibonacci(nth) {
 console.log(fibonacci(1));       // 1
 console.log(fibonacci(2));       // 1
 console.log(fibonacci(3));       // 2
-console.log( fibonacci(4));       // 3
-console.log( fibonacci(5));       // 5
+console.log(fibonacci(4));       // 3
+console.log(fibonacci(5));       // 5
+console.log(fibonacci(6))        // 8
 console.log(fibonacci(12));      // 144
 console.log(fibonacci(20));      // 6765
---
+++ js210_small_problems/08_medium1/08_fib_memoization.js
@@ -0,0 +1,26 @@
+const fibKey = {
+  4: 3,
+  5: 5,
+  6: 8,
+}
+
+function fibonacci(nth) {
+  if (nth <= 2) {
+    return 1;
+  } else if (fibKey[nth]) {
+    return fibKey[nth];
+  } else {
+    fibKey[nth] = fibonacci(nth - 1) + fibonacci(nth - 2);
+  }
+  console.log(fibKey)
+  return fibKey[nth];
+}
+
+console.log(fibonacci(1));       // 1
+console.log(fibonacci(2));       // 1
+console.log(fibonacci(3));       // 2
+console.log(fibonacci(4));       // 3
+console.log(fibonacci(5));       // 5
+console.log(fibonacci(6))        // 8
+console.log(fibonacci(12));      // 144
+console.log(fibonacci(20));      // 6765
